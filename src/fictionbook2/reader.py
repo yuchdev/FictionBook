c5c006e01fe1c9acacba6e7809c742b192ff3e65
Yurii Cherkasov <strategarius@protonmail.com>
2024-01-02 16:37:35 +0700

    Cover image

--- src/fictionbook2/reader.py
+++ src/fictionbook2/reader.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
-import xml.etree.ElementTree as et
+import base64
+import os
+import urllib.request
+import urllib.error
+from xml.etree.ElementTree import parse
 
 
 class Fb2Reader:
-    def __init__(self, file_path, images_dir):
+    """
+    FictionBook2 reader
+    """
+
+    def __init__(self, file_path: str, images_dir: str):
+        """
+        :param file_path:
+        :param images_dir:
+        """
+        if not isinstance(file_path, str):
+            raise TypeError("file_path must be a string")
+        if not isinstance(images_dir, str):
+            raise TypeError("images_dir must be a string")
         self.file_path = file_path
         self.images_dir = images_dir
         self.metadata = {}
@@ -11,14 +27,24 @@ class Fb2Reader:
         self.paragraphs = []
         self.cover_image = None
 
-    def chapter_count(self):
-        return len(self.chapters)
+    def read(self, download_images=False):
+        tree = parse(self.file_path)
+        root = tree.getroot()
+
+        self._extract_metadata(root)
+        self._extract_chapters(root)
+        self.paragraphs = self._chapters_to_paragraphs(self.chapters)
+        self._extract_images(root)
+        if download_images:
+            self._download_images(root)
 
     def _chapters_to_paragraphs(self, chapters):
         """
         Convert list of chapters to lost of paragraphs (strings ended with EOL)
         :return: list of paragraphs
         """
+        if not isinstance(chapters, list):
+            raise TypeError("chapters must be a list")
         paragraphs = []
         for chapter in chapters:
             if isinstance(chapter, list):
@@ -30,50 +56,100 @@ class Fb2Reader:
                 paragraphs.append(chapter)
         return paragraphs
 
-    def read(self):
-        tree = et.parse(self.file_path)
-        root = tree.getroot()
-
-        self._extract_metadata(root)
-        self._extract_chapters(root)
-        self.paragraphs = self._chapters_to_paragraphs(self.chapters)
-
     def _extract_metadata(self, root):
+        """
+        Extract metadata from the root element
+        :param root: xml.etree.ElementTree.Element pointing to the root element of metadata
+        """
         description_tag = root.find(".//{http://www.gribuser.ru/xml/fictionbook/2.0}description")
         title_info_tag = description_tag.find(".//{http://www.gribuser.ru/xml/fictionbook/2.0}title-info")
 
         # Extract metadata properties recursively
-        self._extract_metadata_properties(title_info_tag, self.metadata)
+        self._extract_metadata_properties(title_info_tag)
 
-    def _extract_metadata_properties(self, parent_elem, metadata_dict):
+    def _extract_metadata_properties(self, parent_elem):
+        """
+        Convert metadata properties recursively from xml.etree.ElementTree.Element to dict
+        :param parent_elem: xml.etree.ElementTree.Element pointing to the parent element of metadata
+        :return:
+        """
         for elem in parent_elem:
             if len(elem) > 0:
                 # Recursively set nested properties
                 sub_dict = {}
                 # clean elem.tag from namespace
                 elem.tag = elem.tag.split("}")[1]
-                metadata_dict[elem.tag] = sub_dict
-                self._extract_metadata_properties(elem, sub_dict)
+                self.metadata[elem.tag] = sub_dict
+                self._extract_metadata_properties(elem)
             else:
                 # clean elem.tag from namespace
                 elem.tag = elem.tag.split("}")[1]
                 # Set current property
-                metadata_dict[elem.tag] = elem.text.strip() if elem.text else ""
+                self.metadata[elem.tag] = elem.text.strip() if elem.text else ""
 
             # Handle cover image
-            if elem.tag.endswith("coverpage") and elem.find(
-                    ".//{http://www.gribuser.ru/xml/fictionbook/2.0}image") is not None:
-                image_elem = elem.find(".//{http://www.gribuser.ru/xml/fictionbook/2.0}image")
-                href_attr = image_elem.attrib.get("{http://www.w3.org/1999/xlink}href")
-                if href_attr is not None:
-                    self.cover_image = href_attr[1:]  # Remove the '#' character
+            if elem.tag.endswith("coverpage"):
+                self._extract_cover(elem)
+
+    def _extract_cover(self, elem):
+        # Find the image element directly within the coverpage
+        if (image_elem := elem.find("image")) is not None:
+            if (href_attr := image_elem.attrib.get("{http://www.w3.org/1999/xlink}href")) is not None:
+                # Remove the # character
+                self.cover_image = href_attr[1:]
 
     def _extract_chapters(self, root):
         body_tag = root.find(".//{http://www.gribuser.ru/xml/fictionbook/2.0}body")
-
         for section_tag in body_tag.findall(".//{http://www.gribuser.ru/xml/fictionbook/2.0}section"):
             chapter = []
             for p_tag in section_tag.findall(".//{http://www.gribuser.ru/xml/fictionbook/2.0}p"):
                 paragraph = p_tag.text.strip() if p_tag.text else ""
                 chapter.append(paragraph)
             self.chapters.append(chapter)
+
+    def _extract_images(self, root):
+        binary_elements = root.findall(".//{http://www.gribuser.ru/xml/fictionbook/2.0}binary")
+        for binary_elem in binary_elements:
+            content_type = binary_elem.attrib.get("content-type", "")
+            image_data = binary_elem.text.strip() if binary_elem.text else ""
+            image_id = binary_elem.attrib.get("id", "")
+
+            if image_data and image_id and content_type.startswith("image/"):
+                # Save binary images using the specified id
+                self._save_image_from_binary(image_data, content_type, image_id)
+
+    def _save_image_from_binary(self, image_data, content_type, image_id):
+        image_extension = content_type.split("/")[-1]
+        image_name, ext = os.path.splitext(image_id)
+
+        if not ext:
+            ext = f".{image_extension.lower()}"
+
+        image_path = os.path.join(self.images_dir, image_name + ext)
+
+        with open(image_path, 'wb') as image_file:
+            image_file.write(base64.b64decode(image_data))
+
+    def _download_images(self, root):
+        """
+        Download images from the book if <image l:href="https..."> tag is used
+        and points to URL in the internet
+        """
+        image_elements = root.findall(".//{http://www.gribuser.ru/xml/fictionbook/2.0}image")
+        for image_elem in image_elements:
+            href_attr = image_elem.attrib.get("{http://www.w3.org/1999/xlink}href", "")
+            if href_attr.startswith("http"):
+                # Download images using the specified href
+                self._save_image_from_url(image_url=href_attr)
+
+    def _save_image_from_url(self, image_url):
+        try:
+            with urllib.request.urlopen(image_url) as response:
+                if response.code == 200:
+                    image_name = os.path.basename(image_url)
+                    image_path = os.path.join(self.images_dir, image_name)
+
+                    with open(image_path, 'wb') as image_file:
+                        image_file.write(response.read())
+        except urllib.error.URLError as e:
+            print(f"Error downloading image from {image_url}: {e}")
--- test/fictionbook2_read_test.py
+++ test/fictionbook2_read_test.py
@@ -1,13 +1,43 @@
-import json
 import os
 import unittest
-from unittest.mock import patch, MagicMock
+from fictionbook2.reader import Fb2Reader
 
 
 class Fictionbook2ReaderTest(unittest.TestCase):
 
-    def test_initial(self):
-        pass
+    TEST_ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets')
+
+    def test_metadata(self):
+        test_book_path = os.path.join(self.TEST_ASSETS_PATH, '2020_Sol_Invictus_Book1.fb2')
+        expected_metadata = {
+            'genre': 'prose_contemporary',
+            'author': {'first-name': 'Виктор', 'middle-name': 'Олегович', 'last-name': 'Пелевин'},
+            'book-title': 'Непобедимое солнце. Книга 1',
+            'annotation': {
+                'p': 'Какой стала Саша после встречи с тайной, вы узнаете из книги. Какой стала тайна после встречи с Сашей, вы уже немного в курсе и так.'
+            },
+            'keywords': 'постмодернизм,тайна,философская проза,духовные поиски,современное искусство',
+            'date': '2020',
+            'coverpage': {'image': ''},
+            'lang': 'ru',
+            'sequence': ''
+        }
+        expected_chapters = 2
+        expected_paragraphs = 2596
+
+        reader = Fb2Reader(test_book_path, images_dir='./images')
+        reader.read()
+        self.assertEqual(reader.metadata, expected_metadata)
+        self.assertEqual(len(reader.chapters), expected_chapters)
+        self.assertEqual(len(reader.paragraphs), expected_paragraphs)
+
+    def test_images(self):
+        test_book_path = os.path.join(self.TEST_ASSETS_PATH, '2020_Sol_Invictus_Book1.fb2')
+        expected_cover_image = 'images/cover.jpg'
+
+        reader = Fb2Reader(test_book_path, images_dir='./images')
+        reader.read()
+        self.assertEqual(reader.cover_image, expected_cover_image)
 
 
 if __name__ == '__main__':
